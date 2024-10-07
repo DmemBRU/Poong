2230437edfec7ee7245ee9fd029611df6b2fc347
DmemBRU <danielrodrigo2004@gmail.com>
2024-10-07 14:24:51 +0200

    NJKJKHJK

--- Assets/Scripts/ball.cs
+++ Assets/Scripts/ball.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class ball : MonoBehaviour
 {
+    [SerializeField] private float fuerza;
+    int puntuacion_Player1;
+    int puntuacion_Player2;
     // Start is called before the first frame update
     void Start()
     {
-        transform.Translate(new Vector3( 1, 0, 0)* 1);
+        //GetComponent: para referisrse a los componentes del gameobject (en este caso el rigibody de la bola)
+        //impulso inicial a la bola
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(1,1).normalized * fuerza, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
+    }
+    //se ejecuta el void EL FRAME EN EL QUE la bola EMPIEZA A
+    //atraviesa una zona de trigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Porteria 1") 
+        {
+            Debug.Log("GOL JUGADOR 1");
+            puntuacion_Player1 ++;
+        }
+        else if (collision.tag == "Porteria 2")
+        {
+            Debug.Log("GOL JUGADOR 2");
+            puntuacion_Player2 ++;
+        }
     }
 }
